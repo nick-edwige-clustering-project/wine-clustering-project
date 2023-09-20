974d536413ed8c8fc10d02adad892b39c737094a
eelysee <135763902+eelysee@users.noreply.github.com>
2023-09-20 16:31:32 -0500

    viz plotting funtions for continuos varibales

--- viz.py
+++ viz.py
@@ -14,12 +14,31 @@ def cont_plots(df, cont):
                 plt.xlabel(i.capitalize())
                 plt.ylabel(l.capitalize())
                 plt.show()
+
                 
-for i in cont:
-    l = residual_sugar
-    if i != l:
-        sns.scatterplot(data=df, x=i , y=l)
-        plt.title(f'{l.capitalize()} by {i.capitalize()}')
-        plt.xlabel(i.capitalize())
-        plt.ylabel(l.capitalize())
-        plt.show()
+def contnious_plot(df, cont,target):
+    '''
+    plots target continous variable against all other continuos varibles.
+    '''
+    for i in cont:
+        l = target
+        if i != l:
+            sns.scatterplot(data=df, x=i , y=l)
+            plt.title(f'{l.capitalize()} by {i.capitalize()}')
+            plt.xlabel(i.capitalize())
+            plt.ylabel(l.capitalize())
+            plt.show()
+
+            
+def cont_plots(df, cont):
+    '''
+    scatter plot for all continous
+    '''
+    for i in cont:
+        for l in cont:
+            if i != l:
+                sns.scatterplot(data=df, x=i , y=l)
+                plt.title(f'{l.capitalize()} by {i.capitalize()}')
+                plt.xlabel(i.capitalize())
+                plt.ylabel(l.capitalize())
+                plt.show()
